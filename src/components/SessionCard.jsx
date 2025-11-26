f0e52f857d7c8e5d7d30e8758366a04b1df1c8bc
LaraBerenguer <laraberenguer@outlook.com>
2025-11-26 14:07:37 +0100

    Add dynamic button in SessionCard.jsx

--- src/components/SessionCard.jsx
+++ src/components/SessionCard.jsx
@@ -2,7 +2,8 @@ import { useSchedule } from "../context/ScheduleContext";
 
 const SessionCard = ({ session }) => {
 
-    const { add, sessionIds } = useSchedule();
+    const { add, remove, sessionIds } = useSchedule();
+    const isScheduled = sessionIds.includes(session.id);
     return (
         <div id="session-card" className="border border-black rounded-lg p-4 max-w-80 my-2 flex flex-col gap-2">
             <div className="flex flex-col gap-2">
@@ -15,8 +16,10 @@ const SessionCard = ({ session }) => {
                 {session.duration}
             </div>
             <div className="session-button">
-                <button className="bg-purple-900 text-white px-2 py-1 rounded-lg hover:bg-black hover:cursor-pointer" onClick={() => add(session.id)}>
-                    Add to Schedule
+                <button className={`${isScheduled ? 'bg-red-600' : 'bg-purple-900'} text-white px-2 py-1 rounded-lg hover:bg-black hover:cursor-pointer`}
+                    onClick={isScheduled ? () => remove(session.id) : () => add(session.id)}
+                >
+                    {isScheduled ? "Remove from schedule" : "Add to Schedule"}
                 </button>
             </div>
         </div>
