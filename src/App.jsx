c8d4ee90039dcce76382fd522f104a9a5d2ced23
LaraBerenguer <laraberenguer@outlook.com>
2025-11-26 12:10:05 +0100

    Refactor App component for routing and wrap app with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-//import './App.css'
-import SessionsPage from './pages/SessionsPage'
+//import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SessionsPage from './pages/SessionsPage';
+import MySchedulePage from './pages/MySchedulePage';
+import FormPage from './pages/FormPage';
 
 function App() {
 
   return (
-    <>
-    <SessionsPage />
-    </>
+    <Routes>
+      <Route path="/" element={<SessionsPage />} />
+      <Route path="/my-schedule" element={<MySchedulePage />} />
+      <Route path="/register" element={<FormPage />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
