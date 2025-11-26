671c6347d6e3d376bc4a9210796c1382b4dbfb05
LaraBerenguer <laraberenguer@outlook.com>
2025-11-26 18:51:22 +0100

    Change back to global context for navbar count on MyShedule page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 //import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import SessionsPage from './pages/SessionsPage';
+import SearchPage from './pages/SearchPage';
 import MySchedulePage from './pages/MySchedulePage';
 import FormPage from './pages/FormPage';
 import Navbar from './layout/Navbar';
-import ScheduleLayout from './layout/ScheduleLayout';
 
 function App() {
+
   return (
     <div className="min-h-screen">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
         <Routes>
-          <Route path="/" element={<ScheduleLayout />}>
-            <Route index element={<SessionsPage />} />
-            <Route path="/my-schedule" element={<MySchedulePage />} />
-          </Route>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/my-schedule" element={<MySchedulePage />} />
           <Route path="/register" element={<FormPage />} />
         </Routes>
       </main>
     </div>
   )
-};
+}
 
 export default App;
